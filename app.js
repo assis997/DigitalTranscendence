94cc2e85b8f2fa29cb2d43511f54e6fd3ba77a44
jonathanriese <jonathanriese@gmail.com>
2021-05-08 20:03:27 +0200

    deleted unneccessary js

--- app.js
+++ app.js
@@ -9,21 +9,3 @@ function moveVid() {
     let video = document.getElementById("bgvid");
     video.classList.add("moveUp");
 }
-
-function hideRedirect() {
-    document.getElementById("appBrowser").classList.add("hide");
-}
-
-function isFacebookApp() {
-    var ua = navigator.userAgent || navigator.vendor || window.opera;
-    return (ua.indexOf('Instagram') > -1);
-}
-
-if (isFacebookApp == true) {
-    console.log("app");
-}
-
-if (isFacebookApp !== true) {
-    console.log("no app");
-    hideRedirect();
-}
